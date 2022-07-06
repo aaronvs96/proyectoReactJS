5f9da1c96cf0b25634d26b097075d7934dc09cfd
Aarón Vásquez Sánchez <30508004+aaronvs96@users.noreply.github.com>
2022-07-06 18:07:37 -0500

    mejorando web

--- app-ecomerce/src/App.jsx
+++ app-ecomerce/src/App.jsx
@@ -7,7 +7,12 @@ import 'bootstrap/dist/css/bootstrap.min.css'; //importando boostrap
 
 function App() { // este es un componente de react
 
-  
+  const url = '../assets/data_personas.json'
+  fetch(url)
+  .then( resp =>resp.json())
+  .then( data => console.log(data))
+  .catch( error => console.log(error))
+
   return ( //un componente siempre retorna un jsx
     <div className='App'>
       <NavBar />
