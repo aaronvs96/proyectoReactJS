d6f5daa64d67c037df402ce99417fd5197830044
aaronvs96 <aaronvs.1996@gmail.com>
2022-07-06 23:28:30 -0500

    mejorando web 06.07

--- app-ecomerce/src/App.jsx
+++ app-ecomerce/src/App.jsx
@@ -3,20 +3,38 @@ import ItemListContainer from './components/container/ItemListContainer';
 
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'; //importando boostrap
+import { useEffect, useState } from 'react';
 
 
 function App() { // este es un componente de react
 
-  const url = '../assets/data_personas.json'
-  fetch(url)
-  .then( resp =>resp.json())
-  .then( data => console.log(data))
-  .catch( error => console.log(error))
+  const getFetchApi = async (url) => {
+    try {
+      const query = await fetch(url) //query captura la respuesta del fetch
+      const queryParse = await query.json()//parsear la respuesta
+
+      console.table(queryParse.results)
+
+    } catch (error) {
+      console.log(error)
+    }
+
+    // .then(resp => resp.json())
+    // .then(data => console.table(data))
+    // .catch(error => console.log(error))
+  }
+
+  useEffect(() => {
+    // const url = '../assets/data_personas.json'
+    const url = 'https://pokeapi.co/api/v2/pokemon/?offset=20'
+    getFetchApi(url);
+  }, [])
+
 
   return ( //un componente siempre retorna un jsx
     <div className='App'>
       <NavBar />
-      <ItemListContainer  />
+      <ItemListContainer />
     </div>
   )
 }
